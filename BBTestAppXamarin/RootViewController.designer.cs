c03940e6bbff54c1e0cfc6e708ba38fe8848723f
Christopher Stott <christopherstott@gmail.com>
2015-10-15 13:32:59 -0700

    Initial commit

---
+++ BBTestAppXamarin/RootViewController.designer.cs
@@ -0,0 +1,14 @@
+﻿using Foundation;
+using System.CodeDom.Compiler;
+
+namespace BBTestAppXamarin
+{
+	[Register ("RootViewController")]
+	partial class RootViewController
+	{
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
+
